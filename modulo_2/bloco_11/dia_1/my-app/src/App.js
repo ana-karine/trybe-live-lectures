ec42db3b0614a90a7ebda58da956cd7e5ce70af5
Ana Karine <anakarine.ti@gmail.com>
2020-11-07 13:28:48 -0300

    my app

---
+++ modulo_2/bloco_11/dia_1/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './HelloWorld';
+import HelloWorld2ORetorno from './HelloWorld2ORetorno';
+
+function App() {
+  return (
+    <div className='hello-world'>
+      <HelloWorld />
+      <HelloWorld2ORetorno />
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo_2/bloco_11/dia_1/my-app/src/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (<span>HelloWorld de um componente. </span>)
+  }
+}
+
+export default HelloWorld;
